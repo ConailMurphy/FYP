00ca2edecc3d426ea2ced2a81fb890cb418f2def
Conail Murphy <conailm27@gmail.com>
2019-04-06 05:26:58 +0100

    Added a popup window that lets users choose a list to create a subtree from if they enter a list with more than 3 numbers

--- GUI.py
+++ GUI.py
@@ -1,47 +1,11 @@
 import Tkinter
 import itertools
+import string
 import SortingAlgorithms
 import DecisionTree
 
-# runs a sorting algorithm if user input is valid
-def call_sorting_algorithm(algorithm, array):
-    dt = DecisionTree.DecisionTree()
-    array_permutations = []  # empty list to be filled with all permutations of a given list of elements
-    for perm in (itertools.permutations(array)):
-        # the permutations returned by itertools.permutations are tuples
-        # for the purposes of sorting these permutations need to be in the form of lists
-        permutation = []  # a single permutation
-        for p in perm:
-            permutation.append(p)
-        # a permutation should only be added the the list of all permutations if it is not already in the list
-        # this is possible if the list contains multiple occurrences of the same element
-        if permutation not in array_permutations:
-            array_permutations.append(permutation)
 
-    results = []
-    for a in array_permutations:
-        if algorithm == option_list[0]:
-            results = SortingAlgorithms.bubble_sort(a)
-        elif algorithm == option_list[1]:
-            results = SortingAlgorithms.insertion_sort(a)
-        elif algorithm == option_list[2]:
-            results = SortingAlgorithms.selection_sort(a)
-        elif algorithm == option_list[3]:
-            results = SortingAlgorithms.merge_sort(a)
-        elif algorithm == option_list[4]:
-            results = SortingAlgorithms.heap_sort(a)
-        elif algorithm == option_list[5]:
-            results = SortingAlgorithms.quick_sort(a, 0, len(a) - 1, 0)
-
-        elements = str(a[0]) + " , " + str(a[1]) + " , " + str(a[2])
-        sorted_order = results[0][1] + results[1][1] + results[2][1]
-
-        for l in dt.leaves:
-            if l.j_order == sorted_order:
-                DecisionTree.append_data(l, elements)
-    results_label_text.set((DecisionTree.RenderTree(dt.root)))
-
-# checks that user has submitted valid unput
+# checks that user has submitted valid input
 def submit_user_input():
     feedback_label_text.set("")
     user_input = user_input_entry.get()
@@ -51,41 +15,137 @@ def submit_user_input():
     # check that user input only contains number
     try:
         list_to_sort = []
+        # check that user only entered whole numbers
         for v in user_values:
-            list_to_sort.append(float(v))
-        if len(list_to_sort) != 3:
-            raise IndexError
+            list_to_sort.append(int(v))
+        # checks that user entered at least 3 numbers
+        if len(list_to_sort) < 3:
+            raise IOError
+        if len(list_to_sort) > 3:
+            combinations = []
+            for combination in itertools.combinations(list_to_sort, r=3):
+                c = []
+                for i in combination:
+                    c.append(i)
+                if c not in combinations:
+                    combinations.append(c)
+            list_to_sort = subtree_popup(combinations)
+
         feedback_label_text.set("Success")
-        call_sorting_algorithm(option_var.get(), list_to_sort)
+        call_sorting_algorithm(algorithm_option_var.get(), list_to_sort)
 
     # if non-numeric values are given, prompt the user to try again
     except ValueError:
-        feedback_label_text.set("Please enter numbers only")
+        feedback_label_text.set("Please enter whole numbers only")
         return
 
     # if less than three numbers are given, prompt the user to try again
-    except IndexError:
-        feedback_label_text.set("Please enter 3 numbers")
+    except IOError:
+        feedback_label_text.set("Please enter at least 3 numbers")
         return
 
 
+def subtree_popup(array):
+    index = [0]
+
+    def select_subtree():
+        index[0] = subtree_option_list.index(subtree_option_var.get())
+        popup.quit()
+
+    subtree_option_list = []
+    for i in array:
+        st = ""
+        for j in i:
+            st = st + str(j) + " "
+        subtree_option_list.append(st)
+    popup = Tkinter.Tk()
+    popup.title("Sub-tree Selection")
+
+    popup_label = Tkinter.Label(popup, text="Select a list to create a sub-tree from")
+    popup_label.pack()
+
+    # option list for selecting a list to make a sub-tree from
+    subtree_option_var = Tkinter.StringVar()
+    subtree_option_var.set(subtree_option_list[0])
+    subtree_drop_menu = Tkinter.OptionMenu(popup, subtree_option_var, *subtree_option_list)
+    subtree_drop_menu.grid(column=0, row=1)
+    subtree_drop_menu.pack()
+
+    select_button = Tkinter.Button(popup, text="Select", command=select_subtree)
+    select_button.pack()
+
+    popup.mainloop()
+    popup.destroy()
+    return array[index[0]]
+
+
+
+# runs a sorting algorithm if user input is valid
+def call_sorting_algorithm(algorithm, array):
+    letters = ""
+    for i in range(len(array)):
+        letters = letters + string.ascii_lowercase[i]
+
+    array_permutations = []  # empty list to be filled with all permutations of a given list of elements
+    for permutation in (itertools.permutations(array)):
+        # the permutations returned by itertools.permutations are tuples
+        # for the purposes of sorting these permutations need to be in the form of lists
+        p = []  # a single permutation
+        char = 0
+        for i in permutation:
+            p.append((i, string.ascii_lowercase[char]))
+            char += 1
+        # a permutation should only be added the the list of all permutations if it is not already in the list
+        # this is possible if the list contains multiple occurrences of the same element
+        if p not in array_permutations:
+            array_permutations.append(p)
+
+    order = []
+    for i in range(len(array)):
+        order.append(string.ascii_lowercase[i])
+
+    root = DecisionTree.TreeNode(None, order, None)
+
+    for a in array_permutations:
+        SortingAlgorithms.decisions_made[:] = []
+        unsorted_array = list(a)
+        if algorithm == algorithm_option_list[0]:
+            SortingAlgorithms.bubble_sort(a)
+        elif algorithm == algorithm_option_list[1]:
+            SortingAlgorithms.insertion_sort(a)
+        elif algorithm == algorithm_option_list[2]:
+            SortingAlgorithms.selection_sort(a)
+        elif algorithm == algorithm_option_list[3]:
+            SortingAlgorithms.merge_sort(a)
+        elif algorithm == algorithm_option_list[4]:
+            SortingAlgorithms.heap_sort(a)
+        elif algorithm == algorithm_option_list[5]:
+            SortingAlgorithms.quick_sort(a, 0, len(a) - 1)
+
+        root.add_node(order, SortingAlgorithms.decisions_made, unsorted_array)
+
+    graphic_root = root.create_graphic_node(len(array))
+    results_label_text.set((DecisionTree.RenderTree(graphic_root)))
+
+
+# creating the window that contains all Tkinter widgets
 window = Tkinter.Tk()
 window.title("GUI")
-window.geometry("900x500")
+window.geometry("700x500")
 
-
+# label to prompt the user to select a sorting algorithm
 selection_label_text = Tkinter.StringVar()
 selection_label_text.set("Select a sorting Algorithm")
 selection_label = Tkinter.Label(window, textvariable=selection_label_text)
 selection_label.pack()
 
 # option list for selecting a sorting algorithm to run
-option_list = ["Bubble Sort", "Insertion Sort", "Selection Sort", "Merge Sort", "Heap Sort", "Quick Sort"]
-option_var = Tkinter.StringVar()
-option_var.set(option_list[0])
-drop_menu = Tkinter.OptionMenu(window, option_var, *option_list)
-drop_menu.grid(column=0, row=6)
-drop_menu.pack()
+algorithm_option_list = ["Bubble Sort", "Insertion Sort", "Selection Sort", "Merge Sort", "Heap Sort", "Quick Sort"]
+algorithm_option_var = Tkinter.StringVar()
+algorithm_option_var.set(algorithm_option_list[0])  # bubble sort is the default algorithm
+algorithm_drop_menu = Tkinter.OptionMenu(window, algorithm_option_var, *algorithm_option_list)
+algorithm_drop_menu.grid(column=0, row=6)
+algorithm_drop_menu.pack()
 
 # label to prompt for the user to enter input
 entry_prompt_label_text = Tkinter.StringVar()
@@ -106,6 +166,7 @@ user_input_entry.pack()
 # submit button
 submit_user_input_button = Tkinter.Button(window, text="Submit", command=submit_user_input)
 submit_user_input_button.pack()
+
 
 # label to display the decision tree
 results_label_text = Tkinter.StringVar()
