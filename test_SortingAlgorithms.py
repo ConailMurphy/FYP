54d5b92ddc155e9913178ddb9530b2f249ae0b14
Conail Murphy <conailm27@gmail.com>
2019-04-07 18:56:48 +0100

    Added more unit tests

--- test_SortingAlgorithms.py
+++ test_SortingAlgorithms.py
@@ -6,88 +6,97 @@ class TestSortingAlgorithms(TestCase):
 
     def setUp(self):
         print "Set up"
-        self.lst = [(2, "a"), (1, "b"), (3, "c")]
+        self.list_1 = [(2, "a"), (1, "b"), (3, "c")]
+        self.list_2 = [2, 1, 3]
 
     def tearDown(self):
         print "Tear down", "\n"
 
     def test_swap(self):
         print "Testing swap"
-        x = int(self.lst[0][0])
-        y = int(self.lst[1][0])
-        sA.swap(self.lst, 0, 1)
-        self.assertNotEqual(x, self.lst[0][0])
-        self.assertNotEqual(y, self.lst[0][1])
+        x = int(self.list_1[0][0])
+        y = int(self.list_1[1][0])
+        sA.swap(self.list_1, 0, 1)
+        self.assertNotEqual(x, self.list_1[0][0])
+        self.assertNotEqual(y, self.list_1[0][1])
+        self.assertRaises(IndexError, sA.swap, self.list_1, 0, 3)
 
     def test_bubble_sort(self):
         print "Bubble Sort"
         is_sorted = True
-        self.lst = sA.bubble_sort(self.lst)
-        for i in range(len(self.lst)-1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.bubble_sort(self.list_1)
+        for i in range(len(self.list_1)-1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.bubble_sort, self.list_2)
 
     def test_insertion_sort(self):
         print "Testing Insertion Sort"
         is_sorted = True
-        self.lst = sA.insertion_sort(self.lst)
-        for i in range(len(self.lst)-1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.insertion_sort(self.list_1)
+        for i in range(len(self.list_1)-1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.insertion_sort, self.list_2)
 
     def test_selection_sort(self):
         print "Testing Selection Sort"
         is_sorted = True
-        self.lst = sA.selection_sort(self.lst)
-        for i in range(len(self.lst)-1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.selection_sort(self.list_1)
+        for i in range(len(self.list_1)-1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.selection_sort, self.list_2)
 
     def test_merge_sort(self):
         print "Testing Merge Sort"
         is_sorted = True
-        self.lst = sA.merge_sort(self.lst)
-        for i in range(len(self.lst)-1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.merge_sort(self.list_1)
+        for i in range(len(self.list_1)-1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.merge_sort, self.list_2)
 
     def test_quick_sort(self):
         print "Testing Quick Sort"
         is_sorted = True
-        self.lst = sA.quick_sort(self.lst, 0, len(self.lst) - 1)
-        for i in range(len(self.lst) - 1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.quick_sort(self.list_1, 0, len(self.list_1) - 1)
+        for i in range(len(self.list_1) - 1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.quick_sort, self.list_2, 0, len(self.list_2) - 1)
 
     def test_heap_sort(self):
         print "Testing Heap Sort"
         is_sorted = True
-        self.lst = sA.heap_sort(self.lst)
-        for i in range(len(self.lst)-1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.heap_sort(self.list_1)
+        for i in range(len(self.list_1)-1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.heap_sort, self.list_2)
 
     def test_comb_sort(self):
         print "Testing Comb Sort"
         is_sorted = True
-        self.lst = sA.comb_sort(self.lst)
-        for i in range(len(self.lst)-1):
-            if not self.lst[i][0] <= self.lst[i + 1][0]:
+        self.list_1 = sA.comb_sort(self.list_1)
+        for i in range(len(self.list_1)-1):
+            if not self.list_1[i][0] <= self.list_1[i + 1][0]:
                 is_sorted = False
                 break
         self.assertTrue(is_sorted)
+        self.assertRaises(TypeError, sA.comb_sort, self.list_2)
 
 
 if __name__ == '__main__':
