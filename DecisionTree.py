96db4bd17329ecef4065deb89dd237c1014af4ef
Conail Murphy <conailm27@gmail.com>
2019-04-08 01:08:12 +0100

    Updated the add_node function to check for "swap" or "no swap when creating nodes",fixed an error in the add_leaf_node_data function

--- DecisionTree.py
+++ DecisionTree.py
@@ -28,8 +28,9 @@ class TreeNode:
                 index_b = i
 
         if decisions[0][1] == ">":
-            # order should be changed to show that the algorithm swapped the elements that were compared
-            swap(order, index_a, index_b)
+            if decisions[0][3] == "swap":
+                # order should be changed to show that the algorithm swapped the elements that were compared
+                swap(order, index_a, index_b)
             # if the node does not have a left child, create one
             if self.left_child is None:
                 self.left_child = TreeNode(self, order, string.join(decisions[0]))
@@ -45,7 +46,9 @@ class TreeNode:
                 decisions.pop(0)
                 self.left_child.add_node(order, decisions, data)
         else:
-            # algorithm did not swap elements so there is no change of order
+            if decisions[0][3] == "swap":
+                # order should be changed to show that the algorithm swapped the elements that were compared
+                swap(order, index_a, index_b)
             # if the node does not have a right child, create one
             if self.right_child is None:
                 self.right_child = TreeNode(self, order, string.join(decisions[0]))
@@ -63,28 +66,8 @@ class TreeNode:
 
     # adds a leaf node to a tree
     def add_leaf_node_data(self, decisions, data):
-        a = (data[0], "a")
-        b = (data[1], "b")
-        c = (data[2], "c")
 
-        operand_1 = None
-        operand_2 = None
-
-        if decisions[0][0] == a[1]:
-            operand_1 = a[0]
-        elif decisions[0][0] == b[1]:
-            operand_1 = b[0]
-        elif decisions[0][0] == c[1]:
-            operand_1 = c[0]
-
-        if decisions[0][2] == a[1]:
-            operand_2 = a[0]
-        elif decisions[0][2] == b[1]:
-            operand_2 = b[0]
-        elif decisions[0][2] == c[1]:
-            operand_2 = c[0]
-
-        if operand_1 > operand_2:
+        if decisions[0][1] == ">":
             # update the order and create a left_child
             order = list(self.order)
             index_a = 0
@@ -95,11 +78,13 @@ class TreeNode:
                 elif self.order[i] == decisions[0][2]:
                     index_b = i
             swap(order, index_a, index_b)
-            self. left_child = TreeNode(self, order, string.join(decisions[0]))
+            if not self.left_child:
+                self.left_child = TreeNode(self, order, string.join(decisions[0]))
             self.left_child.data.append(data)
         else:
-            # create a right_child
-            self.right_child = TreeNode(self, self.order, string.join(decisions[0]))
+            # leave order unchanged and create a right_child
+            if not self.right_child:
+                self.right_child = TreeNode(self, self.order, string.join(decisions[0]))
             self.right_child.data.append(data)
 
     # prints the contents of a tree using the node that called the function as the root of the tree
@@ -131,7 +116,7 @@ class TreeNode:
                 if i < graphic_node - 1:
                     order = order + " "
             # anytree's RenderTree function prints the attributes names in alphabetical order
-            # for this reason attribute names have a letter addeed to the start so tha they
+            # for this reason attribute names have a letter added to the start so tha they
             # are printed in the desired order
             new_graphic_node = AnyNode(parent=self.parent, a_decision=None, b_order=order, c_data=data)
         else:
